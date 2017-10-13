806d1a032bd3af83af3d08adf6ffed053ef860a3
Marius van der Wijden <m.vanderwijden@live.de>
2017-10-13 13:24:34 +0200

    added usage info

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -5,11 +5,26 @@ import tools.ExceptionHandler;
 public class ClientMain {
 
     public static void main(String[] args){
-        String ip = args[0];
-        int port = Integer.valueOf(args[1]);
-        String hostDir = args[2];
-        ExceptionHandler.InfoMessage("Starting Client");
-        new Client(ip,port,hostDir);
+        if(args.length != 3) {
+            printHelp();
+            System.exit(1);
+        }
+        try {
+            String ip = args[0];
+            int port = Integer.valueOf(args[1]);
+            String hostDir = args[2];
+            ExceptionHandler.InfoMessage("Starting Client");
+            new Client(ip, port, hostDir);
+        }catch (Exception e){
+            printHelp();
+        }
+    }
+
+    private static void printHelp(){
+        ExceptionHandler.InfoMessage("Usage:");
+        ExceptionHandler.InfoMessage("first argument: ip of server");
+        ExceptionHandler.InfoMessage("second argument: port");
+        ExceptionHandler.InfoMessage("second argument: directory to be synced");
     }
 
 }
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -5,9 +5,24 @@ import tools.ExceptionHandler;
 public class ServerMain {
 
     public static void main(String args[]){
-        int port = Integer.valueOf(args[0]);
-        String hostDir = args[1];
-        ExceptionHandler.InfoMessage("Starting Server");
-        new Server(port,hostDir);
+        if(args.length != 2) {
+            printHelp();
+            System.exit(1);
+        }
+        try {
+            int port = Integer.valueOf(args[0]);
+            String hostDir = args[1];
+            ExceptionHandler.InfoMessage("Starting Server");
+            new Server(port,hostDir);
+        }catch (Exception e){
+            printHelp();
+        }
+    }
+
+
+    private static void printHelp(){
+        ExceptionHandler.InfoMessage("Usage:");
+        ExceptionHandler.InfoMessage("first argument: port");
+        ExceptionHandler.InfoMessage("second argument: directory to save files");
     }
 }
